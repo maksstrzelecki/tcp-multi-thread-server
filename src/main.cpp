c6eb0a43d4c461dcca5ed34846b3d663605f518f
maksstrzelecki <strzeleckimaks4@gmail.com>
2026-07-09 12:56:01 +0200

    Added thread pool implementation and starting concurrent version of TCP

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+
+class ThreadPool {
+public:
+    ThreadPool(size_t threads_num = std::thread::hardware_concurrency()) {
+        for (size_t i = 0; i < threads_num; ++i) {
+            threads_.emplace_back([this] {
+                while (true) {
+                    std::function<void()> task;
+                    {
+                        std::unique_lock<std::mutex> lock(mtx_);
+
+                        cv_.wait(lock, [this] {
+                            return !jobQueue_.empty() || stop_flag_;
+                        });
+
+                        if (jobQueue_.empty() && stop_flag_) return;
+
+                        task = std::move(jobQueue_.front());
+                        jobQueue_.pop();
+                    }   
+                    task();
+                }
+            });
+        }
+    }
+
+    ~ThreadPool() {
+        {
+            std::unique_lock<std::mutex> lock(mtx_);
+            stop_flag_ = true;
+        }
+        cv_.notify_all();
+
+        for (auto& t : threads_) t.join();
+    }
+
+    void add_task(std::function<void()> task) {
+        {
+            std::unique_lock<std::mutex> lock(mtx_);
+            jobQueue_.emplace(std::move(task));
+        }
+        cv_.notify_one();
+    }
+
+private:
+    std::vector<std::thread> threads_;
+    std::queue<std::function<void()>> jobQueue_;
+    std::mutex mtx_;
+    std::condition_variable cv_;
+    bool stop_flag_ = false;
+};
 
 
 int main() {
@@ -26,37 +82,52 @@ int main() {
         perror("listen");
         return 1;
     }
+
+    // creating a thread pool
+    ThreadPool pool;
+
     std::cout << "Server listening on port 8080\n";
 
 
-
-    sockaddr_in client{};
-    socklen_t client_size = sizeof(client);
-
-    int client_fd = accept(server_fd, (sockaddr*)&client, &client_size);
-
-    if (client_fd < 0) {
-        perror("accept");
-        return 1;
-    }
-
-    std::cout << "Client connected\n";
-
-
-
-    char buffer[1024];
     while (true) {
+        sockaddr_in client{};
+        socklen_t client_size = sizeof(client);
 
-        memset(buffer, 0, sizeof(buffer));
+        int client_fd;
+        if ((client_fd = accept(server_fd, (sockaddr*)&client, &client_size)) < 0)
+            perror("accept");
+            break;
 
-        int bytes;
-        if ((bytes = recv(client_fd, buffer, sizeof(buffer), 0)) <= 0) break;
+        pool.add_task([client_fd] {
+            char buffer[1024];
+            memset(buffer, 0, sizeof(buffer));
 
-        std::cout << "Client: " << buffer << "\n";
+            std::cout << "Client\n";
 
-        send(client_fd, buffer, bytes, 0);
+            int recv_bytes;
+            while ((recv_bytes = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) {
+                std::string message(buffer, recv_bytes);
+                std::cout << message;
+            }
+
+            std::string response = "Hello from server\n";
+
+            int sent_bytes_overall = 0;
+            int sent_bytes;
+            while (sent_bytes_overall < response.size()) {
+                sent_bytes = send(
+                    client_fd,
+                    response.data() + sent_bytes_overall,
+                    response.size() - sent_bytes_overall,
+                    0
+                );
+
+                if (sent_bytes <= 0) break;
+                sent_bytes_overall += sent_bytes;
+            }
+            close(client_fd);
+        });
     }
-    close(client_fd);
     close(server_fd);
     return 0;
 }
